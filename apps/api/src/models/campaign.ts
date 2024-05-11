983981d3f972ce73f10e3b81d73c1e2722e01431
John Patrick Madrigal <jp.madrigal07@gmail.com>
2024-05-11 10:36:54 +0800

    feat(api): Modified campaign schema model

--- apps/api/src/models/campaign.ts
+++ apps/api/src/models/campaign.ts
@@ -1,5 +1,25 @@
 import mongoose, { Schema } from "mongoose";
 
+const Patterns = new Schema({
+  name:{
+    type: String,
+    required:true
+  },
+  text:{
+    type:String,
+    required:true
+  }
+})
+
+const Leads = new Schema({
+  values:{
+    type:Object,
+    required:true
+  },
+  paymentRecord: Array,
+  remarks:[String]
+})
+
 const campaign  = new Schema({
   title: {
     type: String,
@@ -7,8 +27,10 @@ const campaign  = new Schema({
   },
   description: {
     type: String,
-    require: true
+    required: true
   },
+  patterns:[Patterns],
+  leads:[Leads],
   createdAt: {
     type: Date,
     required: false,
