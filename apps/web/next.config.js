78fb601d5ed4058f1ed0e8e0a7a70df740d54c7f
Kylle Madrigal <kv.madrigal08@gmail.com>
2024-05-14 10:25:59 +0800

    fix: add rewrite for api origin

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   transpilePackages: ["@repo/ui"],
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:9000/api/:path*',
+      },
+    ];
+  },
 };
