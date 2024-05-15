7ee0bcc857722de8179e53869f562e6757d2e9c5
John Patrick Madrigal <jp.madrigal07@gmail.com>
2024-05-15 16:39:57 +0800

    feat: Hide remarks if no result

--- apps/web/modules/lead/components/basic-information/remarks.tsx
+++ apps/web/modules/lead/components/basic-information/remarks.tsx
@@ -59,77 +59,82 @@ function Remarks({
 
   return (
     <div className="w-full flex flex-col gap-12">
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="flex flex-col gap-2">
-          <label className="text-sm">Remarks</label>
-          <Textarea
-            {...register("comment", { required: true })}
-            disabled={isUpdateCampaignLeadLoading}
-            placeholder="Enter your remarks here"
-            className="h-32"
-          />
-          <div className="flex justify-end">
-            <Button disabled={isUpdateCampaignLeadLoading} className="w-1/6">
-              Add
-            </Button>
-          </div>
-        </div>
-      </form>
-      <div className="flex flex-col gap-4 max-h-96 overflow-auto">
-        {isLoading ? (
-          <div>
-            <div className="p-4">
-              <div className="flex flex-col gap-2">
-                <Skeleton className="h-3 w-32" />
-                <Skeleton className="h-4 w-64" />
+      {campaignLead && (
+        <>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="flex flex-col gap-2">
+              <label className="text-sm">Remarks</label>
+              <Textarea
+                {...register("comment", { required: true })}
+                disabled={isUpdateCampaignLeadLoading}
+                placeholder="Enter your remarks here"
+                className="h-32"
+              />
+              <div className="flex justify-end">
+                <Button disabled={isUpdateCampaignLeadLoading} className="w-1/6">
+                  Add
+                </Button>
               </div>
             </div>
-            <Separator className="h-0.5" />
-            <div className="p-4">
-              <div className="flex flex-col gap-2">
-                <Skeleton className="h-3 w-32" />
-                <Skeleton className="h-4 w-64" />
-              </div>
-            </div>
-            <Separator className="h-0.5" />
-            <div className="p-4">
-              <div className="flex flex-col gap-2">
-                <Skeleton className="h-3 w-32" />
-                <Skeleton className="h-4 w-64" />
-              </div>
-            </div>
-          </div>
-        ) : (
-          <>
-            {campaignLead ? (
-              campaignLead.remarks
-                .sort((a, b) => {
-                  const dateA = new Date(a.date).getTime();
-                  const dateB = new Date(b.date).getTime();
-                  return dateB - dateA;
-                })
-                .map((remark, index) => (
-                  <div key={index}>
-                    <div className="p-4">
-                      <div className="text-xs text-gray-500">
-                        {format(new Date(remark.date), "MMMM d, yyyy h:mm a")}
-                      </div>
-                      <div>{remark.comment}</div>
-                    </div>
-                    {/* Render Separator only if it's not the last item */}
-                    {index !== campaignLead.remarks.length - 1 && (
-                      <Separator className="h-0.5" />
-                    )}
+          </form>
+
+          <div className="flex flex-col gap-4 max-h-96 overflow-auto">
+            {isLoading ? (
+              <div>
+                <div className="p-4">
+                  <div className="flex flex-col gap-2">
+                    <Skeleton className="h-3 w-32" />
+                    <Skeleton className="h-4 w-64" />
                   </div>
-                ))
-            ) : (
-              <div className="text-gray-500">
-                No remarks yet, add one using the form above.
+                </div>
+                <Separator className="h-0.5" />
+                <div className="p-4">
+                  <div className="flex flex-col gap-2">
+                    <Skeleton className="h-3 w-32" />
+                    <Skeleton className="h-4 w-64" />
+                  </div>
+                </div>
+                <Separator className="h-0.5" />
+                <div className="p-4">
+                  <div className="flex flex-col gap-2">
+                    <Skeleton className="h-3 w-32" />
+                    <Skeleton className="h-4 w-64" />
+                  </div>
+                </div>
               </div>
+            ) : (
+              <>
+                {campaignLead ? (
+                  campaignLead.remarks
+                    .sort((a, b) => {
+                      const dateA = new Date(a.date).getTime();
+                      const dateB = new Date(b.date).getTime();
+                      return dateB - dateA;
+                    })
+                    .map((remark, index) => (
+                      <div key={index}>
+                        <div className="p-4">
+                          <div className="text-xs text-gray-500">
+                            {format(new Date(remark.date), "MMMM d, yyyy h:mm a")}
+                          </div>
+                          <div>{remark.comment}</div>
+                        </div>
+                        {/* Render Separator only if it's not the last item */}
+                        {index !== campaignLead.remarks.length - 1 && (
+                          <Separator className="h-0.5" />
+                        )}
+                      </div>
+                    ))
+                ) : (
+                  <div className="text-gray-500">
+                    No remarks yet, add one using the form above.
+                  </div>
+                )}
+              </>
             )}
-          </>
-        )}
-      </div>
+          </div>
+        </>
+      )}
     </div>
   );
 }
--- apps/web/modules/search/index.tsx
+++ apps/web/modules/search/index.tsx
@@ -1,12 +1,25 @@
-import React from "react";
+"use client"
+import React, { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 
 const Search = () => {
+  const router = useRouter();
+  const [search, setSearch] = useState("");
+  const onSubmit = (e: any) => {
+    e.preventDefault();
+    if(search) {
+      router.push(`/lead/${search}`)
+    } else {
+      toast.error("Please add search phrase")
+    }
+  }
   return (
     <div className="space-y-6 p-10 pb-16">
-      <form className="flex w-full max-w-sm items-center space-x-2">
-        <Input type="text" placeholder="Search by ID" autoFocus />
+      <form className="flex w-full max-w-sm items-center space-x-2" onSubmit={onSubmit}>
+        <Input type="text" placeholder="Search by ID" autoFocus onChange={(e) => setSearch(e.target.value)} />
         <Button type="submit">Search</Button>
       </form>
     </div>
